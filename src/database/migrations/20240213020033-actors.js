efd77f3ae5d17e6660749ba87ddd917df3b4fa89
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-02-12 23:47:40 -0300

    adicionando tabela de actors

--- src/database/migrations/20240213020033-actors.js
+++ src/database/migrations/20240213020033-actors.js
@@ -1,10 +1,57 @@
 'use strict';
 
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, _Sequelize) {
+    /**
+    *
+    * @param {import('sequelize').QueryInterface} queryInterface
+    * @param {*} Sequelize
+    */
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable('actors', {
-      
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name:{
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      age:{
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      movies: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      moviesID: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'movies_id',
+        references:{
+          model: 'movies',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      studio:{
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      createdAT: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updateAt: {
+        allowNull:false,
+        type: Sequelize.DATE
+      }
     });
   },
 
